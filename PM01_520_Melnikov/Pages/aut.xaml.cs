a6e1f4fdd086e2c62a12938b9e8ef03fb97a43eb
208556 <208556@KB37-1-120-C02>
2024-04-15 17:17:38 +0300

    Добавьте файлы проекта.

---
+++ PM01_520_Melnikov/Pages/aut.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PM01_520_Melnikov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для aut.xaml
+    /// </summary>
+    public partial class aut : Page
+    {
+        public aut()
+        {
+            InitializeComponent();
+        }
+
+        private void Entr_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Reg_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
